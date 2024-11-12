9129313576b42be2e3e59f7dc5f0b64968f47f6e
DannaSBeltran <143957110+DannaSBeltran@users.noreply.github.com>
2024-11-12 17:30:06 -0500

    Update expresionesirregulares.py

--- expresionesirregulares.py
+++ expresionesirregulares.py
@@ -1,15 +1,29 @@
-import re,json
+import re
+import json
 
-pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(\d{1,2}\/\w{1,3}\/\d{1,4}):(\d{1,2}:\d{1,2}:\d{1,2}) \+\d{1,4}\] \"(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS) (.*)\?(.*) HTTP\/\d.\d\" (\d{3,3})" "/http:(.*)'
 
-with open('apache_logs.txt','r') as lines:
-    jsonExtract= []
+pattern = (
+    r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(\d{1,2}\/\w{1,3}\/\d{1,4}:\d{1,2}:\d{1,2}:\d{1,2}) \+\d{1,4}\] "(GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH) (.*?) HTTP\/\d\.\d" (\d{3}) (\d+) "(.*?)" "(.*?)"')
+
+with open('apache_logs.txt', 'r') as lines:
+    jsonExtract = []
     for line in lines:
-        values = re.finditer(pattern,line)
+        values = re.finditer(pattern, line)
         for match in values:
-            jsonData = {"IP": match.group(1),"Date": match.group(2),'Time':match.group(3), "Method":match.group(4),"Path":match.group(5),"Parameters":match.group(6), "errorcode": match.group(7),"muestra":match.group(8)}
+            jsonData = {
+                "IP": match.group(1),  
+                "date": match.group(2),
+                "Method": match.group(3),
+                "URL": match.group(4),
+                "status": match.group(5),
+                "size": match.group(6),
+                "referer": match.group(7),
+                "nav": match.group(8)
+            }
             jsonExtract.append(jsonData)
-    print(json.dumps(jsonExtract, indent = 4))
+
+    print(json.dumps(jsonExtract, indent=4))
+
 
 
 
